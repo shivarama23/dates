34a8384d185b02f92a2e22c69a8abe5098815603
shivarama23 <shivarama23@gmail.com>
2023-07-17 09:56:17 +0530

    Update dates_regex.py

--- dates_regex.py
+++ dates_regex.py
@@ -5,3 +5,8 @@ def check_exp_expiration(input_string):
     pattern = r"(?<![a-zA-Z0-9_])(exp|expiration)(?![a-zA-Z0-9_])"
     matches = re.findall(pattern, input_string)
     return bool(matches)
+
+def replace_exp_with_space(input_string):
+    pattern = r"EXP(?=Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)"
+    replaced_string = re.sub(pattern, "EXP ", input_string)
+    return replaced_string
