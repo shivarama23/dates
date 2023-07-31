e1868b0f76f8d496a648ae2a76fdc28ac6c0ce6e
shivarama23 <shivarama23@gmail.com>
2023-07-31 12:06:11 +0530

    Update dates_regex.py

--- dates_regex.py
+++ dates_regex.py
@@ -10,3 +10,15 @@ def replace_exp_with_space(input_string):
     pattern = r"EXP(?=Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)"
     replaced_string = re.sub(pattern, "EXP ", input_string)
     return replaced_string
+
+import re
+
+text = "Some text-11oct22-more text-45dec21-even more text."
+
+# Define the regex pattern
+pattern = r'-(\d{2}(?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)\d{2})-'
+
+# Replace the matched pattern with spaces and the captured content
+result = re.sub(pattern, r'  \1 ', text)
+
+print(result)
